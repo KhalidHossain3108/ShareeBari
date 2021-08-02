698429fd742cd9c34d2a821f77adcbefda9917c1
KhalidHossain3108 <86224418+KhalidHossain3108@users.noreply.github.com>
2021-08-02 19:46:44 +0600

    Update and rename a1.html to index.php

---
+++ index.php
@@ -0,0 +1,413 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+
+<title>Sharee Bari</title>
+<!-- -->
+<!--linking bootstarp -->
+<!--font awesome-->
+<script src="https://kit.fontawesome.com/3a09b235a1.js" crossorigin="anonymous"></script>
+
+<!--whatsapp+ social media button link -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+  
+<!--links for fonts -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Mina&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Teko:wght@300&display=swap" rel="stylesheet">
+<link href="https://fonts.maateen.me/charukola-ultra-light/font.css" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Zen+Loop&display=swap" rel="stylesheet">
+
+<style type="text/css">
+/* */
+/* style for collapsible button */
+.collapsible {
+  
+  background-color: #3EBCAF;
+  color: #444;
+  cursor: pointer;
+  width: 100%;
+  height:150px;
+  border: none;
+  padding:18px;
+  font-size: 100px;
+  font-family:cursive;
+}
+
+/* Add a background color to the button if it is clicked on (add the .active class with JS), and when you move the mouse over it (hover) */
+.active, .collapsible:hover {
+  background-color: #F8A6AC;
+}
+
+/* Style the collapsible content. Note: hidden by default */
+.content {
+  padding:18px;
+  display: none;
+  overflow: hidden;
+  background-color: #DCFEFB;
+  font-size:50px;
+  font-family: 'Teko', sans-serif;
+}
+
+
+
+
+/* style for social media button*/
+.fa {
+padding: 20px;
+font-size: 100px;
+width: 100px;
+height:100px;
+text-align: center;
+text-decoration: none;
+
+border-radius: 50%;
+}
+
+.fa:hover {
+opacity: 0.5;
+}
+
+.fa-facebook {
+background: #3B5998;
+color: white;
+}
+
+.fa-twitter {
+background: #55ACEE;
+color: white;
+}
+
+.fa-google {
+background: #E8021D;
+color: white;
+}
+
+
+
+
+/*style for floating whatsapp */
+.float{
+	position:fixed;
+	width:160px;
+	height:160px;
+	bottom:80px;
+	right:50px;
+	background-color:#25d366;
+	color:#FFF;
+	border-radius:50%;
+	text-align:center;
+  font-size:130px;
+	box-shadow: 4px 4px 3px grey;
+  z-index:100;
+}
+.float1{
+	position:fixed;
+	width:160px;
+	height:160px;
+	bottom:280px;
+	right:50px;
+	
+	color:#FFF;
+	border-radius:50%;
+	text-align:center;
+  font-size:50px;
+	box-shadow: 4px 4px 3px grey;
+  z-index:100;
+}
+
+
+
+
+a{
+text-decoration:none;
+}
+body{
+background-image:url(tbg.jpg);
+background-attachment:fixed;
+}
+.a1{
+position:fixed;
+height:290px;
+width:100%;
+background:#890059 !important;
+z-index:1;
+}
+.a2{
+height:420px;
+width:100%;
+}
+.a3{
+font-size:150px;
+text-shadow:4px 4px 2px grey;
+font-family: 'Zen Loop', cursive;
+letter-spacing:4px;
+}
+.a4{
+font-size:60px;
+font-family: 'Zen Loop', cursive;
+}
+
+.a5{
+ background-color:#C1109A;
+
+width:100%;
+ }
+ .a6{
+ color:white;
+ font-size:30px;
+ font-family:cursive;
+ 
+ !important;
+ }
+ .a7{color:white;
+ font-size:20px;
+ font-family:cursive;}
+ 
+
+</style>
+
+
+
+</head>
+
+
+<body>
+
+
+
+<div class="a1">
+&nbsp&nbsp<br><br><br>
+<table>
+<tr><td>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</td>
+<td ><img src="tlogo.jpeg" height="150px" width="150px"></td>
+<td>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</td>
+<td ><font style=" font-family: 'Mina', sans-serif; font-size:70px; color:white">  Sharee Bari</font></td>
+</tr>
+</table>
+</div>
+
+<hr style="position:fixed" color="840564" size="12">
+
+
+
+
+
+<br><br>
+<br><br><br><br><br><br><br>
+
+<hr color="840564" size="12">
+
+<div class="a2" style="  background-image:url(titem1.jpg) ">
+<marquee scrolldelay="200" scrollamount="70" direction="down" behavior="slide">
+<center><p style=" font-family: 'CharukolaUltraLight', sans-serif;   color:white; font-size:64px">মানুষ পোশাক দ্বারা অঙ্কিত হয়, <br>আমরা পোশাকে আকিঁ। </p></center>
+<br><br><br>
+</marquee>
+</div>
+
+<hr color="840564" size="12">
+<br><br><br><br><br><br>
+
+<hr color="840564" size="12">
+
+<img class="a7" src="tscroll1.jpeg"  height="800px" width="100%">
+
+<hr color="840564" size="12">
+<br><br>
+
+
+
+
+<p class="a3"> Draw yourself now!</p>
+
+
+<a href="">
+<div  style=" height:230px ; width:100%; background-image:url(titem2.jpg) ">
+<center><p style="color:white; font-family: 'CharukolaUltraLight', sans-serif; font-size:150px"> বাটিক</p></center>
+</div> 
+</a>
+<hr color="840564" size="12">
+
+
+
+<a href="">
+
+<div  style=" height:230px ; width:100%; background-image:url(titem3.jpg) ">
+<center><p style="color:white; font-family: 'CharukolaUltraLight', sans-serif; font-size:150px"> ব্লক</p></center>
+</div>
+</a>
+<hr color="840564" size="12">
+
+<div  style=" height:230px ; width:100%; background-image:url(titem2.jpg) ">
+<center><p style="color:white; font-family: 'CharukolaUltraLight', sans-serif; font-size:150px"> মিক্সড </p></center>
+</div>
+</a>
+<hr color="840564" size="12">
+
+<br><br><br><br><br><br><br>
+<hr color="840564" size="12">
+<button type="button" class="collapsible">About Sharee Bari</button>
+<div class="content">
+<p style="font-family: 'Mina', sans-serif">শাড়ি বাড়ি কাজ করছে শাড়ি নিয়ে প্রায় তিন বছরের অভিজ্ঞতা থেকে । আমরা সুতি, সিল্ক শাড়ি নিয়ে কাজ করছি। ব্লক বাটিক ট্যাসেলসহ আরো বিভিন্ন প্রকারের ডিজাইন শোভা পায় আমাদের শাড়িগুলোতে। ব্যতিক্রম, দৃষ্টিনন্দন, আরামদায়ক, টেকসই পোশাক তৈরী আমাদের মূল লক্ষ্য। </p>
+</p> 
+
+</div>
+<hr color="840564" size="12">
+<a href="https://api.whatsapp.com/send?phone=+8801632096380&text=Hi! I'm interested in those designs you do.Can you help me by.. " class="float" target="_blank">
+<i class="fa fa-whatsapp my-float"></i>
+</a>
+<a href="https://m.me/md.khalidhossain.3108" ><img src="tmsngr.jpg" class="float1"></a>
+<br>
+<center>
+<p class="a4">Contact us : <a href="tel:+8801632096380">+8801632096380</a></p>
+<p class="a4">Email : <a href="mailto:touhidaafrin05@gmail.com">touhidaafrin05@gmail.com</a></p>
+<p class="a4">Address : <a href="https://maps.google.com/?cid=9189332423715087464&entry=gps">Polashbari, Savar, Dhaka </a></p>
+<br><br>
+<hr color="840564" size="12">
+<br><br><br>
+<center><a href="fb://facewebmodal/https://m.facebook.com/%E0%A6%B6%E0%A6%BE%E0%A7%9C%E0%A6%BF-%E0%A6%AC%E0%A6%BE%E0%A7%9C%E0%A6%BF-101293351953587/?tsid=0.9916902302718893&source=result" class="fa fa-facebook"></a> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+<a href="tel:01632096380" class="fa fa-twitter"></a>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+<a href="mailto:touhidaafrin05@gmail.com" class="fa fa-google"></a>
+</center>
+<br><br><br>
+
+<hr color="840564" size="12">
+<br><br><br><br>
+<p class="a4"><a href="">Privacy Policy</a></p>
+<p class="a4"><a href="">Terms & Conditions </a></p>
+<br><br><br><br>
+<footer><center>
+<p class="a4">© 2021 | Sharee Bari | All Rights Reserved</p>
+<p class="a4"> Powered by : <a href="fb://facewebmodal/md.khalidhossain.3108"> <span style="font-family:cursive">Khalid Hossain </span></a></p>
+</center> </footer>
+</center>
+<script>
+
+
+/*for collapsible */
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+
+
+
+var x=document.getElementsByTagName("img");
+var y=3;
+
+function f1()
+  {
+y=y+1;
+ if(y%3==0)
+  {
+  x[1].src="tscroll2.jpeg";
+  x[1].alt="First Image";
+  }
+else if(y%3==1)  
+  {
+  x[1].src="tscroll3.jpeg";
+  x[1].alt="Second Image";
+  }
+else
+  {
+  x[1].src="tscroll1.jpeg";
+  x[1].alt="Third Image";
+  }
+
+  }
+  setInterval(f1,3000);
+  
+  
+  var m=document.getElementsByTagName("font");
+  var n=3;
+  
+  function f2()
+  {
+  n=n+1;
+  if(n%2==0)
+  {
+  m[0].innerHTML="শাড়ি বাড়ি"  ;
+  
+  }
+  
+  else
+  {
+   m[0].innerHTML="Sharee Bari" ;
+   
+  }
+  }
+  setInterval(f2,5000);
+  
+  var a=document.getElementsByTagName("p");
+  var b=3;
+  
+  function f3()
+  {
+  b=b+1;
+  if(b%2==1)
+  {
+  a[2].innerHTML="বাটিক"  ;
+  a[3].innerHTML="ব্লক"  ;
+  a[4].innerHTML="মিক্সড"  ;
+  
+  }
+  
+  else
+  {
+  a[2].innerHTML="Batik"  ;
+  a[3].innerHTML="Block"  ;
+  a[4].innerHTML="Mixed"  ;
+  
+  }
+  }
+  setInterval(f3,5000);
+  
+  var a1=document.getElementsByTagName("marquee");
+  var b1=3;
+  
+  function f4()
+  {
+  b1=b1+1;
+  if(b1%2==0)
+  {
+  a1[0].p.innerHTML="শাড়ি বাড়ি"  ;
+  
+  }
+  
+  else
+  {
+  a1[0].p.innerHTML="Sharee Bari" ;
+  
+  }
+  }
+ setInterval(f4,5000)
+  
+
+</script>
+
+<!--js bootstrap -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+</body>
+</html>
