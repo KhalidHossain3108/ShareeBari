06e56ffbced05f584f88d2fc83e7579932958cc3
KhalidHossain3108 <86224418+KhalidHossain3108@users.noreply.github.com>
2021-08-02 19:48:02 +0600

    Update and rename a2.html to a2.php

---
+++ a2.php
@@ -0,0 +1,500 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+
+<title>Sharee Bari</title>
+<!-- -->
+<!--linking bootstarp -->
+<!--font awesome-->
+<script src="https://kit.fontawesome.com/3a09b235a1.js" crossorigin="anonymous"></script>
+
+<!--whatsapp+ social media button link -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+  
+<!--links for fonts -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Hind+Siliguri&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Raleway:wght@200&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Mina&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Teko:wght@300&display=swap" rel="stylesheet">
+<link href="https://fonts.maateen.me/charukola-ultra-light/font.css" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Zen+Loop&display=swap" rel="stylesheet">
+
+<style type="text/css">
+/* */
+/* style for collapsible button */
+.collapsible {
+  
+  background-color: #3EBCAF;
+  color: #444;
+  cursor: pointer;
+  width: 100%;
+  height:150px;
+  border: none;
+  padding:18px;
+  font-size: 100px;
+  font-family:cursive;
+}
+
+/* Add a background color to the button if it is clicked on (add the .active class with JS), and when you move the mouse over it (hover) */
+.active, .collapsible:hover {
+  background-color: #F8A6AC;
+}
+
+/* Style the collapsible content. Note: hidden by default */
+.content {
+  padding:18px;
+  display: none;
+  overflow: hidden;
+  background-color: #DCFEFB;
+  font-size:50px;
+  font-family: 'Teko', sans-serif;
+}
+
+
+
+
+/* style for social media button*/
+.fa {
+padding: 20px;
+font-size: 100px;
+width: 100px;
+height:100px;
+text-align: center;
+text-decoration: none;
+
+border-radius: 50%;
+}
+
+.fa:hover {
+opacity: 0.5;
+}
+
+.fa-facebook {
+background: #3B5998;
+color: white;
+}
+
+.fa-twitter {
+background: #55ACEE;
+color: white;
+}
+
+.fa-google {
+background: #E8021D;
+color: white;
+}
+
+
+
+
+/*style for floating whatsapp */
+.float{
+	position:fixed;
+	width:160px;
+	height:160px;
+	bottom:80px;
+	right:50px;
+	background-color:#25d366;
+	color:#FFF;
+	border-radius:50%;
+	text-align:center;
+  font-size:130px;
+	box-shadow: 4px 4px 3px grey;
+  z-index:100;
+}
+.float1{
+	position:fixed;
+	width:160px;
+	height:160px;
+	bottom:280px;
+	right:50px;
+	
+	color:#FFF;
+	border-radius:50%;
+	text-align:center;
+  font-size:50px;
+	box-shadow: 4px 4px 3px grey;
+  z-index:100;
+}
+
+
+
+
+a{
+text-decoration:none;
+}
+body{
+background-image:url(tbg.jpg);
+background-attachment:fixed;
+}
+.a1{
+position:fixed;
+height:290px;
+width:100%;
+background:#890059 !important;
+z-index:1;
+up:0px;
+}
+.a2{
+height:420px;
+width:100%;
+}
+.a3{
+font-size:70px;
+text-shadow:2px 2px 2px grey;
+font-family: 'Raleway', sans-serif;
+letter-spacing:4px;
+}
+.a4{
+font-size:60px;
+font-family: 'Zen Loop', cursive;
+}
+
+.a5{
+ background-color:#C1109A;
+
+width:100%;
+ }
+ .a6{
+ color:white;
+ font-size:30px;
+ font-family:cursive;
+ 
+ !important;
+ }
+ .a7{
+ width:100%;
+ background-color:#C2FFFB;
+ font-size:50px;
+ font-family: 'Hind Siliguri', sans-serif; }
+
+ 
+
+</style>
+
+
+
+</head>
+
+
+<body>
+ 
+
+<div class="a1">
+&nbsp&nbsp<br><br><br>
+<table>
+<tr><td>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</td>
+<td ><img src="tlogo.jpeg" height="150px" width="150px"></td>
+<td>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</td>
+<td ><font style=" font-family: 'Mina', sans-serif; font-size:70px; color:white">  Sharee Bari</font></td>
+</tr>
+</table>
+</div>
+<hr  color="840564" size="12">
+<br><br><br><br><br><br><br><br><br>
+
+<hr color="840564" size="12">
+<img src="td16.jpg" width="100%" height="800px">
+  <table align="center" class="a7" border="2">
+  <tr ><td style="padding:5px;" style="padding:20px;" colspan="2"> <p>শাড়িবাড়ির নতুন সংযোজন।  </p>
+  </td></tr>
+  <tr ><td><center>Price : ----  Tk</center></td><td><center>Available</center></td></tr>
+  </table>
+<hr color="840564" size="12">
+<br><br><br><br>
+
+
+<hr color="840564" size="12">
+<img src="td1.jpg" width="100%" height="700px">
+  <table align="center" class="a7" border="2">
+  <tr ><td style="padding:5px;" style="padding:20px;" colspan="2"><p> হাল্ফ সিল্ক শাড়ির উপরে জামদানী ডাইসের ব্লক, আঁচলে রয়েছে হাতের কাজ। </p>
+  </td></tr>
+  <tr ><td><center>Price : 1080 Tk</center></td><td><center>Available</center></td></tr>
+  </table>
+<hr color="840564" size="12">
+<br><br><br>
+
+<hr color="840564" size="12">
+<img src="td2.jpg" width="100%" height="700px">
+  <table align="center" class="a7" border="2">
+  <tr ><td style="padding:5px;" style="padding:20px;" colspan="2"><p> হাল্ফ সিল্ক শাড়ির উপর কাঠের ব্লক,পুরো শাড়ি জুড়ে ডলার বসানো আছে, সাথে আছে হাতের কাজ।</p>
+  </td></tr>
+  <tr ><td><center>Price : 1200 Tk</center></td><td><center>Available</center></td></tr>
+  </table>
+<hr color="840564" size="12">
+<br><br><br>
+
+<hr color="840564" size="12">
+<img src="td3.jpg" width="100%" height="700px">
+  <table align="center" class="a7" border="2">
+  <tr ><td style="padding:5px;" style="padding:20px;" colspan="2"><p>হাল্ফ সিল্ক শাড়ির উপর কাঠের ব্লক,পুরো শাড়িতেই ডলার এবং হাতের করা আছে। </p>
+  </td></tr>
+  <tr ><td><center>Price : 1200 Tk</center></td><td><center>Available</center></td></tr>
+  </table>
+<hr color="840564" size="12">
+<br><br><br>
+
+<hr color="840564" size="12">
+<img src="td4.jpg" width="100%" height="800px">
+  <table align="center" class="a7" border="2">
+  <tr ><td style="padding:5px;" style="padding:20px;" colspan="2"> <p>হাল্ফ সিল্ক এর নীল শাড়ি,কাঠের ব্লক এবং হাতের কাজ করা আছে। </p>
+  </td></tr>
+  <tr ><td><center>Price : 980 Tk</center></td><td><center>Available</center></td></tr>
+  </table>
+<hr color="840564" size="12">
+<br><br><br><br>
+
+
+<hr color="840564" size="12">
+<img src="td5.jpg" width="100%" height="700px">
+  <table align="center" class="a7" border="2">
+  <tr ><td style="padding:5px;" style="padding:20px;" colspan="2"><p>হাল্ফ সিল্ক শাড়ির উপর কাঠের ব্লক করা, হাতের কাজ এবং ট্যাসেল করা আছে। </p>
+  </td></tr>
+  <tr ><td><center>Price : 1100 Tk</center></td><td><center>Available</center></td></tr>
+  </table>
+<hr color="840564" size="12">
+<br><br><br>
+
+<hr color="840564" size="12">
+<img src="td6.jpg" width="100%" height="700px">
+  <table align="center" class="a7" border="2">
+  <tr ><td style="padding:5px;" style="padding:20px;" colspan="2"><p>শুভ্র শাড়ি। সাদা হাল্ফ সিল্ক শাড়ির উপর কাঠের ব্লক, সাথে হাতের কাজ করা।  </p>
+  </td></tr>
+  <tr ><td><center>Price : 980 Tk</center></td><td><center>Available</center></td></tr>
+  </table>
+<hr color="840564" size="12">
+<br><br><br>
+
+<hr color="840564" size="12">
+<img src="td7.jpg" width="100%" height="800px">
+  <table align="center" class="a7" border="2">
+  <tr ><td style="padding:5px;" style="padding:20px;" colspan="2"> <p> হাল্ফ সিল্ক শাড়ি,
+  পাড়ে,আঁচলে ব্লক এবং হাতের কাজ করা।</p>
+  </td></tr>
+  <tr ><td><center>Price : 950 Tk</center></td><td><center>Available</center></td></tr>
+  </table>
+<hr color="840564" size="12">
+<br><br><br><br>
+
+
+<hr color="840564" size="12">
+<img src="td8.jpg" width="100%" height="700px">
+  <table align="center" class="a7" border="2">
+  <tr ><td style="padding:5px;" style="padding:20px;" colspan="2"><p>মেরুন হাল্ফ স্লিক শাড়িতে কাঠের বল্ক করা, পুরো শাড়ির আঁচল এবং পাড় জুড়ে আছে হাতের কাজ। </p>
+  </td></tr>
+  <tr ><td><center>Price : 950 Tk</center></td><td><center>Available</center></td></tr>
+  </table>
+<hr color="840564" size="12">
+<br><br><br>
+
+<hr color="840564" size="12">
+<img src="td9.jpg" width="100%" height="700px">
+  <table align="center" class="a7" border="2">
+  <tr ><td style="padding:5px;" style="padding:20px;" colspan="2"><p> সুতি কোটা শাড়ির উপর কাঠের ব্লক করা, শাড়ির পাড়ে আর আচলে হাতের কাজ এবং ডলার বসানো আছে।</p>
+  </td></tr>
+  <tr ><td><center>Price : 1150 Tk</center></td><td><center>Available</center></td></tr>
+  </table>
+<hr color="840564" size="12">
+<br><br><br>
+
+<hr color="840564" size="12">
+<img src="td10.jpg" width="100%" height="800px">
+  <table align="center" class="a7" border="2">
+  <tr ><td style="padding:5px;" style="padding:20px;" colspan="2"> <p> অফ হোয়াইট হাল্ফ সিল্ক শাড়ির উপর কাঠের বল্ক,আচলে হাতের কাজ এবং ট্যাসেল বসানো আছে,বসন্তের ছোয়াও আছে কিছুটা ।</p>
+  </td></tr>
+  <tr ><td><center>Price : 880 Tk</center></td><td><center>Available</center></td></tr>
+  </table>
+<hr color="840564" size="12">
+<br><br><br><br>
+
+
+<hr color="840564" size="12">
+<img src="td11.jpg" width="100%" height="700px">
+  <table align="center" class="a7" border="2">
+  <tr ><td style="padding:5px;" style="padding:20px;" colspan="2"><p>হাল্ফ সিল্ক শাড়ির উপরে কাঠের ব্লক। পুরো আঁচলে সুতা দিয়ে কাজ করা এবং ট্যাসেল বসানো আছে। </p>
+  </td></tr>
+  <tr ><td><center>Price : 880 Tk</center></td><td><center>Available</center></td></tr>
+  </table>
+<hr color="840564" size="12">
+<br><br><br>
+
+<hr color="840564" size="12">
+<img src="td12.jpg" width="100%" height="700px">
+  <table align="center" class="a7" border="2">
+  <tr ><td style="padding:5px;" style="padding:20px;" colspan="2"><p>পিউর সিল্ক শাড়ির উপর ব্লক এবং বাটিক করা,সাথে বাটিক এর ব্লাউজ তো আছেই । </p>
+  </td></tr>
+  <tr ><td><center>Price : 1850 Tk</center></td><td><center>Available</center></td></tr>
+  </table>
+<hr color="840564" size="12">
+<br><br><br>
+
+<hr color="840564" size="12">
+<img src="td13.jpg" width="100%" height="800px">
+  <table align="center" class="a7" border="2">
+  <tr ><td style="padding:5px;" style="padding:20px;" colspan="2"> <p>লাল আর সবুজ হাল্ফ সিল্ক শাড়ির এর উপর কাঠের ব্লক আর হাতের কাজ তো থাকছেই। </p>
+  </td></tr>
+  <tr ><td><center>Price : 980 Tk</center></td><td><center>Available</center></td></tr>
+  </table>
+<hr color="840564" size="12">
+<br><br><br><br>
+
+
+<hr color="840564" size="12">
+<img src="td14.jpg" width="100%" height="700px">
+  <table align="center" class="a7" border="2">
+  <tr ><td style="padding:5px;" style="padding:20px;" colspan="2"><p> আকাশী রং এর হাল্ফ সিল্ক শাড়ির উপর কাঠের ব্লক, পাড় এবং আচঁলে রয়েছে হাতের কাজ ।</p>
+  </td></tr>
+  <tr ><td><center>Price : 950 Tk</center></td><td><center>Available</center></td></tr>
+  </table>
+<hr color="840564" size="12">
+<br><br><br>
+
+<hr color="840564" size="12">
+<img src="td15.jpg" width="100%" height="700px">
+  <table align="center" class="a7" border="2">
+  <tr ><td style="padding:5px;" style="padding:20px;" colspan="2"><p>হাফ সিল্ক শাড়ির উপর কাঠের ব্লক করা,আচলে হাতের কাজ,ডলার এবং ট্যাসেল বসানো আছে, স্বল্প আলো এর কারনে স্নিগ্ধ এই শাড়ি টার  রং টা আনতে পারলাম নাহ।  </p>
+  </td></tr>
+  <tr ><td><center>Price : 980 Tk</center></td><td><center>Available</center></td></tr>
+  </table>
+<hr color="840564" size="12">
+<br><br><br>
+
+
+
+<a href="">
+<div  style=" height:230px ; width:100%; background-image:url(titem2.jpg) ">
+<center><p style="color:white; font-family: 'CharukolaUltraLight', sans-serif; font-size:150px"> বাটিক</p></center>
+</div> 
+</a>
+<hr color="840564" size="12">
+
+
+<a href="">
+
+<div  style=" height:230px ; width:100%; background-image:url(titem3.jpg) ">
+<center><p style="color:white; font-family: 'CharukolaUltraLight', sans-serif; font-size:150px"> ব্লক</p></center>
+</div>
+</a>
+<hr color="840564" size="12">
+
+<div  style=" height:230px ; width:100%; background-image:url(titem2.jpg) ">
+<center><p style="color:white; font-family: 'CharukolaUltraLight', sans-serif; font-size:150px"> মিক্সড </p></center>
+</div>
+</a>
+<hr color="840564" size="12">
+
+<a href="https://api.whatsapp.com/send?phone=+8801632096380&text=Hi! I'm interested in those designs you do.Can you help me by.. " class="float" target="_blank">
+<i class="fa fa-whatsapp my-float"></i>
+</a>
+<a href="https://m.me/md.khalidhossain.3108" ><img src="tmsngr.jpg" class="float1"></a>
+<br>
+<center>
+<p class="a4">Contact us : <a href="tel:+8801632096380">+8801632096380</a></p>
+<p class="a4">Email : <a href="mailto:touhidaafrin05@gmail.com">touhidaafrin05@gmail.com</a></p>
+<p class="a4">Address : <a href="https://maps.google.com/?cid=9189332423715087464&entry=gps">Polashbari, Savar, Dhaka </a></p>
+<br><br>
+<hr color="840564" size="12">
+<br><br><br>
+<center><a href="fb://facewebmodal/md.khalidhossain.3108" class="fa fa-facebook"></a> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+<a href="tel:01632096380" class="fa fa-twitter"></a>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+<a href="mailto:touhidaafrin05@gmail.com" class="fa fa-google"></a>
+</center>
+<br><br><br>
+
+<hr color="840564" size="12">
+<br><br><br><br>
+<p class="a4"><a href="">Privacy Policy</a></p>
+<p class="a4"><a href="">Terms & Conditions </a></p>
+<br><br><br><br>
+<footer><center>
+<p class="a4">© 2021 | Sharee Bari | All Rights Reserved</p>
+<p class="a4"> Powered by : <a href="fb://facewebmodal/md.khalidhossain.3108"> <span style="font-family:cursive">Khalid Hossain </span></a></p>
+</center> </footer>
+</center>
+<script>
+alert("To confirm order please inform us through social media plugins or contact number given below.\n\n\nঅর্ডার করতে সোশ্যাল মিডিয়া প্লাগইন অথবা নিচে দেওয়া নাম্বারে যোগাযোগ করুন। ");
+
+/*for collapsible */
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+
+
+  
+  
+  var m=document.getElementsByTagName("font");
+  var n=3;
+  
+  function f2()
+  {
+  n=n+1;
+  if(n%2==0)
+  {
+  m[0].innerHTML="শাড়ি বাড়ি"  ;
+  
+  }
+  
+  else
+  {
+   m[0].innerHTML="Sharee Bari" ;
+   
+  }
+  }
+  setInterval(f2,5000);
+
+  
+  var a1=document.getElementsByTagName("marquee");
+  var b1=3;
+  
+  function f4()
+  {
+  b1=b1+1;
+  if(b1%2==0)
+  {
+  a1[0].p.innerHTML="শাড়ি বাড়ি"  ;
+  
+  }
+  
+  else
+  {
+  a1[0].p.innerHTML="Sharee Bari" ;
+  
+  }
+  }
+ setInterval(f4,5000)
+  
+
+</script>
+
+<!--js bootstrap -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+</body>
+</html>
